0cf085367f26930b37a17974fbe1879a010b7b69
foundry <foundry.tv@gmail.com>
2023-05-29 02:23:01 +0100

    dependencies specified

--- Package.swift
+++ Package.swift
@@ -17,6 +17,10 @@ let package = Package(
             targets: ["VPKit-dynamic"]),
         
     ],
+    dependencies: [
+        .package(url: "https://github.com/aws-amplify/aws-sdk-ios-spm", from:"2.0.0"),
+        .package(url: "https://gitlab.com/foundry/dotveep-spm/", from:"2.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
