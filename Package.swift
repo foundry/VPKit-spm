2f02e4e5e1cdb90205d0f438c42872e731441e80
foundry <foundry.tv@gmail.com>
2023-05-29 10:40:18 +0100

    refactor

--- Package.swift
+++ Package.swift
@@ -31,85 +31,74 @@ let package = Package(
             dependencies: [
                 .target(name: "VPKit-static-wrapper",
                         condition: .when(platforms: [.iOS])
-                       )],
-            path: "VPKit-static-target"
-            
-        ),
-        .target (
-            name: "VPKit-static-wrapper",
-            dependencies: [
-                .target(name: "VPKit-static-binary",
-                        condition: .when(platforms: [.iOS])),
-//                .target(name: "dotveep-static-binary",
-//                        condition: .when(platforms: [.iOS])),
-                .product(
-                    name: "AWSCore",
-                    package: "aws-sdk-ios-spm"),
-                .product(
-                    name: "AWSKinesis",
-                    package: "aws-sdk-ios-spm"),
-                .product(name: "dotveep-static",
-                         package: "dotveep-spm")
-                
+                       ),
+                .target(name: "AWS-wrapper",
+                        condition: .when(platforms: [.iOS])
+                       )
             ],
-            path: "VPKit-static-wrapper"
+            path: "VPKit-static-target"
         ),
         
-
+            .target (
+                name: "VPKit-static-wrapper",
+                dependencies: [
+                    .target(name: "VPKit-static-binary",
+                            condition: .when(platforms: [.iOS])),
+                    .product(name: "dotveep-static",
+                             package: "dotveep-spm")
+                    
+                ],
+                path: "VPKit-static-wrapper"
+            ),
+        
+        
             .target (
                 name: "VPKit-dynamic-target",
                 dependencies: [
                     .target(name: "VPKit-dynamic-wrapper",
                             condition: .when(platforms: [.iOS])
-                           )],
+                           ),
+                    .target(name: "AWS-wrapper",
+                            condition: .when(platforms: [.iOS])
+                           )
+                ],
                 path: "VPKit-dynamic-target"
             ),
         
-        .target (
-            name: "VPKit-dynamic-wrapper",
-            dependencies: [
-                .target(name: "VPKit-dynamic-binary",
-                        condition: .when(platforms: [.iOS])),
-//                .target(name: "dotveep-dynamic-binary",
-//                        condition: .when(platforms: [.iOS])),
-                .product(
-                    name: "AWSCore",
-                    package: "aws-sdk-ios-spm"),
-                .product(
-                    name: "AWSKinesis",
-                    package: "aws-sdk-ios-spm"),
-                .product(name: "dotveep-dynamic",
-                         package: "dotveep-spm")
-            ],
-            path: "VPKit-dynamic-wrapper"
-        ),
+            .target (
+                name: "VPKit-dynamic-wrapper",
+                dependencies: [
+                    .target(name: "VPKit-dynamic-binary",
+                            condition: .when(platforms: [.iOS])),
+                    .product(name: "dotveep-dynamic",
+                             package: "dotveep-spm")
+                ],
+                path: "VPKit-dynamic-wrapper"
+            ),
         
-            
+            .target (name: "AWS-wrapper",
+                     dependencies: [
+                        .product(
+                            name: "AWSCore",
+                            package: "aws-sdk-ios-spm"),
+                        .product(
+                            name: "AWSKinesis",
+                            package: "aws-sdk-ios-spm")
+                     ],
+                     path: "AWS-wrapper"
+                    ),
+        
             .binaryTarget(
                 name: "VPKit-static-binary",
                 url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-static/VPKit.xcframework.zip",
                 checksum: "c5e8ab406126abf7340d5f02fc7b1cda60a14d53bef894f053501be84460198d"
             ),
-    
         
         
             .binaryTarget(
                 name: "VPKit-dynamic-binary",
                 url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-dynamic/VPKit.xcframework.zip",
                 checksum: "37c9e2b3bdb53191290c528a89b5594bcba0650dc26eebd6b0eb0e0bb10722d0")
-//        ,
-        
-//
-//        .binaryTarget(
-//                name: "dotveep-static-binary",
-//                url: "https://gitlab.com/foundry/dotveep/-/raw/2.0.1/xcframeworks/dotveep-static/dotveep.xcframework.zip",
-//                checksum: "210da9aa6378c805807da4d42c7c7aba46761b755b7aa44fc2155340a25bb2b2"),
-//
-//        .binaryTarget(
-//            name: "dotveep-dynamic-binary",
-//            url: "https://gitlab.com/foundry/dotveep/-/raw/2.0.1/xcframeworks/dotveep-dynamic/dotveep.xcframework.zip",
-//            checksum: "865b74e0f931e9027203e4a29bdbbb9d486efb0ab3425b9fd63fc39d32fee637")
-        
-        
+              
     ]
 )
