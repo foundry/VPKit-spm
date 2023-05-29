c3c017c0fd7ffdfdc0817374184f9c8235ec121e
foundry <foundry.tv@gmail.com>
2023-05-29 03:53:29 +0100

    deps

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-    // swift-tools-version: 5.8
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -14,19 +14,19 @@ let package = Package(
             targets: ["VPKit-static-target"]),
         .library(
             name: "VPKit-dynamic",
-            targets: ["VPKit-dynamic"]),
+            targets: ["VPKit-dynamic-target"]),
         
     ],
-//    dependencies: [
-//        .package(
-//            name:"aws",
-//            url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
-//            from: "2.0.0"),
-//        .package(
-//            name:"dotveep",
-//            url: "https://gitlab.com/foundry/dotveep-spm/",
-//            from: "2.0.0")
-//    ],
+    //    dependencies: [
+    //        .package(
+    //            name:"aws",
+    //            url: "https://github.com/aws-amplify/aws-sdk-ios-spm",
+    //            from: "2.0.0"),
+    //        .package(
+    //            name:"dotveep",
+    //            url: "https://gitlab.com/foundry/dotveep-spm/",
+    //            from: "2.0.0")
+    //    ],
     targets: [
         .target (
             name: "VPKit-static-target",
@@ -35,12 +35,12 @@ let package = Package(
                         condition: .when(platforms: [.iOS])
                        )],
             path: "VPKit-static-target"
-
+            
         ),
         .target (
             name: "VPKit-static-wrapper",
             dependencies: [
-                .target(name: "VPKit-static",
+                .target(name: "VPKit-static-binary",
                         condition: .when(platforms: [.iOS])),
                 .target(name: "dotveep-static-binary",
                         condition: .when(platforms: [.iOS])),
@@ -48,13 +48,7 @@ let package = Package(
             path: "VPKit-static-wrapper"
         ),
         
-        .binaryTarget(
-            name: "VPKit-static",
-            url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-static/VPKit.xcframework.zip",
-            checksum: "c5e8ab406126abf7340d5f02fc7b1cda60a14d53bef894f053501be84460198d"),
-        
-        
-        
+
             .target (
                 name: "VPKit-dynamic-target",
                 dependencies: [
@@ -62,29 +56,38 @@ let package = Package(
                             condition: .when(platforms: [.iOS])
                            )],
                 path: "VPKit-dynamic-target"
-
-            ),
-            .target (
-                name: "VPKit-dynamic-wrapper",
-                dependencies: [
-                    .target(name: "VPKit-dynamic",
-                            condition: .when(platforms: [.iOS])),
-                    .target(name: "dotveep-dynamic-binary",
-                            condition: .when(platforms: [.iOS])),
-                ],
-                path: "VPKit-dynamic-wrapper"
             ),
         
+        .target (
+            name: "VPKit-dynamic-wrapper",
+            dependencies: [
+                .target(name: "VPKit-dynamic-binary",
+                        condition: .when(platforms: [.iOS])),
+                .target(name: "dotveep-dynamic-binary",
+                        condition: .when(platforms: [.iOS])),
+            ],
+            path: "VPKit-dynamic-wrapper"
+        ),
+        
+            
+            .binaryTarget(
+                name: "VPKit-static-binary",
+                url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-static/VPKit.xcframework.zip",
+                checksum: "c5e8ab406126abf7340d5f02fc7b1cda60a14d53bef894f053501be84460198d"
+            ),
+    
         
         
+            .binaryTarget(
+                name: "VPKit-dynamic-binary",
+                url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-dynamic/VPKit.xcframework.zip",
+                checksum: "37c9e2b3bdb53191290c528a89b5594bcba0650dc26eebd6b0eb0e0bb10722d0"),
+        
         .binaryTarget(
-            name: "VPKit-dynamic",
-            url: "https://gitlab.com/foundry/VPKit/-/raw/2.10.0/xcframeworks/VPKit-dynamic/VPKit.xcframework.zip",
-            checksum: "37c9e2b3bdb53191290c528a89b5594bcba0650dc26eebd6b0eb0e0bb10722d0"),
-        .binaryTarget(
-            name: "dotveep-static-binary",
-            url: "https://gitlab.com/foundry/dotveep/-/raw/2.0.1/xcframeworks/dotveep-static/dotveep.xcframework.zip",
-            checksum: "210da9aa6378c805807da4d42c7c7aba46761b755b7aa44fc2155340a25bb2b2"),
+                name: "dotveep-static-binary",
+                url: "https://gitlab.com/foundry/dotveep/-/raw/2.0.1/xcframeworks/dotveep-static/dotveep.xcframework.zip",
+                checksum: "210da9aa6378c805807da4d42c7c7aba46761b755b7aa44fc2155340a25bb2b2"),
+    
         .binaryTarget(
             name: "dotveep-dynamic-binary",
             url: "https://gitlab.com/foundry/dotveep/-/raw/2.0.1/xcframeworks/dotveep-dynamic/dotveep.xcframework.zip",
